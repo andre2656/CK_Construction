af02ae53c5ba308d8b957944636b6ec2998fd639
andre2656 <44176906+andre2656@users.noreply.github.com>
2019-04-29 14:07:28 -0600

    Updated steel page

--- client/src/pages/Steel.js
+++ client/src/pages/Steel.js
@@ -1,28 +1,41 @@
 import React from 'react';
 
-class Steel extends React.Component{
-    render(){
-        return(
-            <div>
-
-From simple structures to complicated components and steel systems, CK Construction has experience and can do it all !!
-                
-                CK Construction is more than a General Contractor and Construction Manager.  We have full time Steel Erection Crews that specialize in steel projects from complicated components and steel systems, Pre-Engineered Metal Buildings, Seismic Upgrades and more....
-                
-                 If it’s got Steel – CK Steel can do the job. 
-                
-                STEEL TEAM MANAGER
-                Rob Checketts   
-                robc@ckconstructioncorp.com
-                Phone: 801-731-0717   
-                Fax: 801-732-8956  
-
-
-                Pre-Engineered to fit your application and budget.
-Since we are licensed in 11 western states, we understand the ins and outs of local building codes and can help our clients interested in metal buildings to select the right materials for the project.
-
-Metal Buildings don't have to be Boxes.
-Thanks to cost-effectiveness and fire-resistance as well as some serious strength, the metal building construction industry is growing faster than any other type of construction, and provides a great solution to almost any construction problem.
+class Steel extends React.Component {
+    render() {
+        return (
+            <div className='container'>
+                <div classname='row'>
+                    <h2>STEEL STRUCTURES</h2>
+                </div>
+                <div classname='row'>
+                    <p>From simple structures to complicated components and steel systems, CK Construction has experience and can do it all !!
+                    </p>
+                </div>
+                <div classname='row'>
+                    <p>CK Construction is more than a General Contractor and Construction Manager.
+                       We have full time Steel Erection Crews that specialize in steel projects from complicated components and steel systems, Pre-Engineered Metal Buildings, Seismic Upgrades and more....
+                    </p>
+                </div>
+                <div classname='row'>
+                    <p>If it’s got Steel – CK Steel can do the job.</p>
+                </div>
+                <div classname='row'>
+                    <strong>STEEL TEAM MANAGER</strong><br />
+                    Rob Checketts<br />
+                    robc@ckconstructioncorp.com<br />
+                    Phone: 801-731-0717<br />
+                    Fax: 801-732-8956<br />
+                </div>
+                <div className='row'>
+                    <p>Pre-Engineered to fit your application and budget.
+                    Since we are licensed in 11 western states, we understand the ins and outs of local building codes and can help our clients interested in metal buildings to select the right materials for the project.
+                    </p>
+                </div>
+                <div classname='row'>
+                    <p>Metal Buildings don't have to be Boxes.
+                       Thanks to cost-effectiveness and fire-resistance as well as some serious strength, the metal building construction industry is growing faster than any other type of construction, and provides a great solution to almost any construction problem.
+                    </p>
+                </div>
             </div>
         )
     }
