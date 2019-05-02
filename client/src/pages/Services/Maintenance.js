39666f8f6331ac5c7edb4c7e0fb2f98df1b5aec2
Andre Campbell <andre2656@gmail.com>
2019-05-01 18:59:07 -0600

    Adding bootstrap to about

--- client/src/pages/Services/Maintenance.js
+++ client/src/pages/Services/Maintenance.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Maintenance = () => {
     return(
-        <div>
-            <h3>Mainternace</h3>
-            <h5>Taking care of business.</h5>
-            <p> Sometimes the toughest part of a project doesn’t start until after it’s finished.
-            At CK Construction our commitment continues if your project requires ongoing maintenance.
-            From helping set up schedules to self-performing services, when your project is finished, we’re just getting started.
-            </p>
+        <div className= 'container'>
+            <div className= 'row'>
+                <div className= 'col-md-5'>
+                    <h3>Mainternace</h3>
+                    <h5>Taking care of business.</h5>
+                    <p> Sometimes the toughest part of a project doesn’t start until after it’s finished.
+                    At CK Construction our commitment continues if your project requires ongoing maintenance.
+                    From helping set up schedules to self-performing services, when your project is finished, we’re just getting started.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
