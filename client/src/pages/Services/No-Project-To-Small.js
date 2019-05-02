60525dc8ba84cff2f6374255d81e892f89475b2d
Andre Campbell <andre2656@gmail.com>
2019-05-01 19:03:25 -0600

    Adding bootstrap to about

--- client/src/pages/Services/No-Project-To-Small.js
+++ client/src/pages/Services/No-Project-To-Small.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const ProjectSmall = () => {
     return (
-        <div>
-            <h3>No Project To Small</h3>
-           <h5>We make the ordinary extraordinary.</h5>
-            <p>Remodel, re-roof, repair—it doesn’t matter what the project is—it’s important to us if it’s important to our clients. 
-                Honestly, we just love construction. 
-                So if it’s a maintenance project, a minor remodel, or you scored some dough for a handful of capital improvements, trust CK Construction to help you make the most of your money.
-            </p>
+        <div className='container'>
+            <div className= 'row'>
+                <div className= 'col-md-6'>
+                    <h3>No Project To Small</h3>
+                    <h5>We make the ordinary extraordinary.</h5>
+                    <p>Remodel, re-roof, repair—it doesn’t matter what the project is—it’s important to us if it’s important to our clients.
+                        Honestly, we just love construction.
+                        So if it’s a maintenance project, a minor remodel, or you scored some dough for a handful of capital improvements, trust CK Construction to help you make the most of your money.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
