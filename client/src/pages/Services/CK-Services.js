d4026835c89c2ea5a9085cf2dac89eef8d61d11c
Andre Campbell <andre2656@gmail.com>
2019-05-01 19:17:29 -0600

    Added Bootstrap to about page

--- client/src/pages/Services/CK-Services.js
+++ client/src/pages/Services/CK-Services.js
@@ -2,21 +2,20 @@ import React from 'react';
 
 const CKServices = () => {
     return (
-        <div>
-            <h3>CK Services</h3>
-            <h5>With a smile…</h5>
-            <p className= 'col-sm-2'>
-            Some contractors say they do it all.
-
-            At CK Construction, we actually do.
-
-            New construction, remodeling,
-
-            steel erection…whatever we’re
-
-            doing, we do it well</p>
-
-
+        <div className= 'container'>
+            <div className= 'row'>
+                <div className= 'col-md-6'>
+                    <h3>CK Services</h3>
+                    <h5>With a smile…</h5>
+                    <p className='col-sm-2'>
+                        Some contractors say they do it all.
+                        At CK Construction, we actually do.
+                        New construction, remodeling,
+                        steel erection…whatever we’re
+                        doing, we do it well
+                     </p>
+                </div>
+            </div>
         </div>
     )
 }
