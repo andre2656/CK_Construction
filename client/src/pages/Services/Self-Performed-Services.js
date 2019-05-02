03f03527a430ea451b03671a6b4f20b6ac8b6bf9
Andre Campbell <andre2656@gmail.com>
2019-05-01 18:47:50 -0600

    Adding bootstrap

--- client/src/pages/Services/Self-Performed-Services.js
+++ client/src/pages/Services/Self-Performed-Services.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const PerformedServices = () => {
     return (
-        <div>
-            <h3>Self Preformed Services</h3>
-            <h5>If you want a job done right…</h5>
-            <p>You know how the old saying goes. 
-                Doing the job right is important to us and to our clients, so at CK Construction we self-perform steel erection, concrete finish work, framing, and installation of all furnishings and fixtures. 
-            </p>
+        <div classNmae= 'container'>
+            <div className= 'row'>
+                <div className= 'col-md-6'>
+                    <h3>Self Preformed Services</h3>
+                    <h5>If you want a job done right…</h5>
+                    <p>You know how the old saying goes.
+                        Doing the job right is important to us and to our clients, so at CK Construction we self-perform steel erection, concrete finish work, framing, and installation of all furnishings and fixtures.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
