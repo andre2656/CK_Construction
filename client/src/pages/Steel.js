4f8ee2797edda684588b4787874f829d19955728
Andre Campbell <andre2656@gmail.com>
2019-05-01 20:14:40 -0600

    Updated Steel page

--- client/src/pages/Steel.js
+++ client/src/pages/Steel.js
@@ -8,32 +8,34 @@ class Steel extends React.Component {
                     <h3>STEEL STRUCTURES</h3>
                 </div>
                 <div classname='row'>
-                    <p>From simple structures to complicated components and steel systems, CK Construction has experience and can do it all !!
+                    <p className= 'col-md-6'>From simple structures to complicated components and steel systems, CK Construction has experience and can do it all !!
                     </p>
                 </div>
                 <div classname='row'>
-                    <p>CK Construction is more than a Construction Manager.
+                    <p className='col-md-6'>CK Construction is more than a Construction Manager.
                        We have full time Steel Erection Crews that specialize in steel projects from complicated components and steel systems, Pre-Engineered Metal Buildings, Seismic Upgrades and more....
                     </p>
                 </div>
                 <div classname='row'>
-                    <p>If it’s got Steel – CK Steel can do the job.</p>
+                    <p> className= 'col-md-6'If it’s got Steel – CK Steel can do the job.</p>
                 </div>
                 <div classname='row'>
-                    <strong>STEEL TEAM MANAGER</strong><br />
-                    Rob Checketts<br />
-                    robc@ckconstructioncorp.com<br />
-                    Phone: 801-731-0717<br />
-                    Fax: 801-732-8956<br />
+                    <div className= 'col-md-6'>
+                        <strong>STEEL TEAM MANAGER</strong><br />
+                        Rob Checketts<br />
+                        robc@ckconstructioncorp.com<br />
+                        Phone: 801-731-0717<br />
+                        Fax: 801-732-8956<br />
+                    </div>
                 </div>
                 <div className='row'>
                     <h5>Pre-Engineered to fit your application and budget.</h5>
-                    <p>Since we are licensed in 11 western states, we understand the ins and outs of local building codes and can help our clients interested in metal buildings to select the right materials for the project.
+                    <p className= 'col-md-6'>Since we are licensed in 11 western states, we understand the ins and outs of local building codes and can help our clients interested in metal buildings to select the right materials for the project.
                     </p>
                 </div>
                 <div classname='row'>
                     <h5>Metal Buildings don't have to be Boxes.</h5>
-                       <p>Thanks to cost-effectiveness and fire-resistance as well as some serious strength, the metal building construction industry is growing faster than any other type of construction, and provides a great solution to almost any construction problem.
+                    <p className= 'col-md-6'>Thanks to cost-effectiveness and fire-resistance as well as some serious strength, the metal building construction industry is growing faster than any other type of construction, and provides a great solution to almost any construction problem.
                     </p>
                 </div>
             </div>
